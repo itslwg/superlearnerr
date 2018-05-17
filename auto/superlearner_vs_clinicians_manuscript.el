de3e37b594c6ec1d8bc0b223aa22fcb6e1d5bc3d
Martin Gerdin <martingerdin@gmail.com>
2018-05-17 08:08:18 +0200

    fixed references
    Former-commit-id: be0408e75126428a80f79456d45f20234b0bfeac

--- auto/superlearner_vs_clinicians_manuscript.el
+++ auto/superlearner_vs_clinicians_manuscript.el
@@ -30,37 +30,7 @@
     "microtype"
     "xcolor"
     "array")
-   (LaTeX-add-bibitems
-    "Brohi2017"
-    "GBD2017"
-    "UN2018"
-    "Yeboah2014"
-    "OReilly2013"
-    "Roy2017"
-    "EAST2010"
-    "NICE2016"
-    "Voskens2018"
-    "Granstrom2018"
-    "ASCOT2014"
-    "Tignanelli2018"
-    "Benjamin2018"
-    "VanRein2018"
-    "SATG2012"
-    "ESI2012"
-    "Baker2013"
-    "Choi2017"
-    "Beam2018"
-    "Nevin2018"
-    "Liu2017"
-    "Talbert2007"
-    "Pearl2008"
-    "Scerbo2014"
-    "Follin2016"
-    "Levin2018"
-    "WHOICD"
-    "GCSAID"
-    "Rehn2011"
-    "R"
-    "SuperLearner"))
+   (LaTeX-add-bibliographies
+    "bibliography"))
  :latex)
 
