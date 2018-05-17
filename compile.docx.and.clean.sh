929448665c4529598a0e12929e4d6c5ed810de0b
Martin Gerdin <martingerdin@gmail.com>
2018-05-17 08:22:54 +0200

    modified to handle version ids in file names
    Former-commit-id: fd0ed62768d2d0d9a2591d82760ee4186fef56d1

--- compile.docx.and.clean.sh
+++ compile.docx.and.clean.sh
@@ -2,4 +2,4 @@
 ## Compile docx using pandoc
 pandoc -s --bibliography=bibliography.bib --csl=plos.csl superlearner_vs_clinicians_manuscript.tex -o superlearner_vs_clinicians_manuscript.docx
 ## Clean directory
-rm superlearner_vs_clinicians_manuscript.aux superlearner_vs_clinicians_manuscript.bbl superlearner_vs_clinicians_manuscript.blg superlearner_vs_clinicians_manuscript.log superlearner_vs_clinicians_manuscript.out
+rm superlearner_vs_clinicians_manuscript_*.aux superlearner_vs_clinicians_manuscript_*.bbl superlearner_vs_clinicians_manuscript_*.blg superlearner_vs_clinicians_manuscript_*.log superlearner_vs_clinicians_manuscript_*.out
