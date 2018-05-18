d9f68b5fbbb1fa6fc99d4e7ed9de6c175a632921
Martin Gerdin <martingerdin@gmail.com>
2018-05-18 10:59:13 +0200

    started adding results to test manuscript

--- R/compile.manuscript.r
+++ R/compile.manuscript.r
@@ -1,16 +1,20 @@
 #' Compile manuscript function
 #'
 #' This function compiles the Superlearner vs clinicians manuscript
+#' @param results_list The list holding all relevant results. No default.
 #' @param manuscript_file_name_prefix The file name prefix of the manuscript as a character vector of length 1. No default.
 #' @param manuscript_file_format The format of the manuscript file. Defaults to "rtex".
 #' @param compiler The compiler to use when compiling the rtex manuscript into a pdf. Defaults to pdflatex.
 #' @export
 compile.manuscript <- function(
+                               results_list,
                                manuscript_file_name_prefix,
                                manuscript_file_format = "rtex",
                                compiler = "pdflatex"
                                )
 {
+    ## Attached results list
+    attach(results_list)
     ## Get list of files matching the manuscript file name prefix
     files <- list.files(pattern = manuscript_file_name_prefix)
     file <- grep(manuscript_file_format, files, value = TRUE)
