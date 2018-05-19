5d7da9c506ec4c7fc8d52b8502b69f6cac82ddca
Martin Gerdin <martingerdin@gmail.com>
2018-05-19 10:56:38 +0200

    started working on adding point estimates to results

--- R/generate.confidence.intervals.r
+++ R/generate.confidence.intervals.r
@@ -1,12 +1,14 @@
 #' Confidence interval function
 #'
 #' This function generates confidence intervals around difference of two given point estimates using empirical bootstrapping.
+#' @param measure The name of the measure to be estimated. No default.
 #' @param study_sample The study sample list. No default.
 #' @param func Function that generates key statistic. For example, model.review.AUROCC that generates AUROCC of a given model, or model.review.reclassification that generates reclassification elements. No default.
 #' @param model_or_pointestimate Character vector describing predictions or point estimates to analyse. For example, pred_cat and clinicians_predictions predictions for model.review.AUROCC or NRI+ and NRI- point estimates for model.review.reclassification. No default.
 #' @param samples Samples as prepared with train.predict.bssamples. No default.
 #' @export
 generate.confidence.intervals <- function(
+                                          measure,
                                           study_sample,
                                           model_or_pointestimate,
                                           func,
@@ -15,6 +17,8 @@ generate.confidence.intervals <- function(
 {
     ## Get point estimates of func
     study_point_estimates <- func(study_sample, model_or_pointestimate)
+    ## Save point estimates to results
+    # results[[paste0(measure, "_point_estimate")]] <<- 
     ## Calculate difference of point estimates
     diff <- study_point_estimates[[1]] - study_point_estimates[[2]]
     ## Generate statistic on every bootstrap samples
