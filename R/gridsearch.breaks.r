68189a0c167c9b5dceb9c7cdd6eacf601cde2510
Martin Gerdin <martingerdin@gmail.com>
2018-05-22 15:16:36 +0200

    running gridsearch in parallel
    Former-commit-id: 0a25fcf0d6fe31205ffde4aa1c2899b584f441ce

--- R/gridsearch.breaks.r
+++ R/gridsearch.breaks.r
@@ -5,7 +5,9 @@
 #' @param outomes A numeric vector of 0 and 1. No default.
 #' @param n_breaks Number of breaks, i.e. cutpoints. Defaults to 3.
 #' @param loss_function The name of the loss function as a character vector. For now any performance measure included in ROCR is accepted. Defaults to "auc".
-#' @param sample Logical. If TRUE loss is only calculated for 10% of possible breaks. Defaults to TRUE. 
+#' @param sample Logical. If TRUE loss is only calculated for 10% of possible breaks. Defaults to TRUE.
+#' @param parallel Logical. If TRUE the gridsearch is done in parallel. Defaults to FALSE.
+#' @param n_cores Integer. The number of cores to use for parallel computing. Defaults to NULL.
 #' @param save_plot Logical. If TRUE a wireframe plot is saved to disk. Defaults to FALSE.
 #' @param verbose Logical. If TRUE then information is printed as the gridsearch runs. Defaults to FALSE. Currently ignored.
 #' @export
@@ -15,6 +17,8 @@ gridsearch.breaks <- function(
                               n_breaks = 3,
                               loss_function = "auc",
                               sample = TRUE,
+                              parallel = FALSE,
+                              n_cores = NULL,
                               save_plot = FALSE,
                               verbose = FALSE
                               )
@@ -31,14 +35,28 @@ gridsearch.breaks <- function(
     breaks_to_search <- combn(seq(0.01, 0.99, 0.01), n_breaks, simplify = FALSE)
     ## Draw a random sample if sample is TRUE
     if (sample) breaks_to_search <- breaks_to_search[sample(1:length(breaks_to_search), ceiling(length(breaks_to_search) * 0.1))]
-    ## Do gridsearch
-    loss_list <- lapply(breaks_to_search, function(breaks) {
+    ## Define estimate loss function
+    estimate.loss <- function(breaks) {
         breaks <- c(-Inf, breaks, Inf)
         groupings <- as.numeric(cut(predictions, breaks = breaks))
         rocr_pred <- ROCR::prediction(groupings, outcomes)
         loss <- loss_functions[[loss_function]](rocr_pred)
         return(loss)
-    })
+    }
+    ## Do gridsearch
+    if (parallel) {
+        if (is.null(n_cores)) {
+            message("You have not specified the number of cores so 2 will be used")
+            n_cores <- 2
+        }
+        parallel_breaks <- split(breaks_to_search, cut(1:length(breaks_to_search), breaks = n_cores, include.lowest = TRUE))
+        cl <- makeCluster(n_cores)
+        registerDoParallel(cl)
+        message("Running gridsearch for optimal cutpoints in parallel on ", n_cores, " cores")
+        parallel_list <- foreach(breaks_to_search = parallel_breaks) %dopar% lapply(breaks_to_search, estimate.loss)
+        stopCluster(cl)
+        loss_list <- do.call(c, parallel_list)
+    } else loss_list <- lapply(breaks_to_search, estimate.loss)
     ## Loss data
     loss_data <- data.frame(do.call(rbind, breaks_to_search), loss = unlist(loss_list))
     ## Make and save plot
--- R/predictions.with.superlearner.r
+++ R/predictions.with.superlearner.r
@@ -1,15 +1,17 @@
 #' Generate predictions with SuperLearner
 #'
 #' This function trains SuperLearner on the training set and makes predictions on the review set. Then, predictions are divided by quantiles into four colour-coded groups. The groups are green, yellow, orange, and red. They respectively include ranges from the 0% quantile to 25% quantile, 25% to 50%, 50% to 75%, and 75% to 100% of the continous predictions. (To be changed)
-#' @param prepped_data Data as prepared by prep.data.for.superlearner as list. No default.
+#' @param prepped_sample Data as prepared by prep.data.for.superlearner as list. No default.
 #' @param models Models to use in ensemble algorithm. Default: SL.mean and SL.glmnet.
 #' @param save_breaks Logical. If TRUE, save optimal breaks to results. Defaults to FALSE.
 #' @param save_all_predictions Logical. If TRUE all predictions are saved in pred_data. Defaults to FALSE.
 #' @param sample Logical. If TRUE the grid search will only search a random sample of possible cutpoint combinations, not all. Defaults to TRUE.
+#' @param gridsearch_parallel Logical. If TRUE the gridsearch is performed in parallel. Defaults to FALSE.
+#' @param n_cores Integer. The number of cores to run any parallel computing on. Default to NULL.
 #' @param verbose Logical. If TRUE information to help gauge progress is printed. Defaults to TRUE.
 #' @export
 predictions.with.superlearner <- function(
-                                          prepped_data,
+                                          prepped_sample,
                                           models = c('SL.glmnet',
                                                      'SL.glm',
                                                      'SL.randomForest',
@@ -18,18 +20,20 @@ predictions.with.superlearner <- function(
                                           save_breaks = FALSE,
                                           save_all_predictions = FALSE,
                                           sample = TRUE,
+                                          gridsearch_parallel = FALSE,
+                                          n_cores = NULL,
                                           verbose = TRUE
                                           )
 {
     ## Train algorithm with training set
     if (verbose) message("Training SuperLearner")
-    train_algo <- with(prepped_data, SuperLearner(Y = y_train,
+    train_algo <- with(prepped_sample, SuperLearner(Y = y_train,
                                                   X = x_train,
                                                   family = binomial(),
                                                   SL.library = models))
     if (verbose) message("SuperLearner trained")
     ## Predict with algorithm on training set
-    sets <- list(train = prepped_data$x_train, test = prepped_data$x_review)
+    sets <- list(train = prepped_sample$x_train, test = prepped_sample$x_review)
     continuous_predictions <- lapply(sets, function(sample) {
         predict(train_algo,
                 sample,
@@ -39,7 +43,11 @@ predictions.with.superlearner <- function(
     train_pred <- continuous_predictions$train
     ## Do a grid search to find optimal cutpoints
     if (verbose) message("Finding optimal cutpoints")
-    breaks <- gridsearch.breaks(predictions = train_pred, outcomes = prepped_data$y_train, sample = sample)
+    breaks <- gridsearch.breaks(predictions = train_pred,
+                                outcomes = prepped_sample$y_train,
+                                sample = sample,
+                                parallel = gridsearch_parallel,
+                                n_cores = n_cores)
     if (verbose) message("Optimal cutpoints identified")
     ## Save breaks
     if (save_breaks) results$optimal_breaks <<- breaks
@@ -54,13 +62,13 @@ predictions.with.superlearner <- function(
     ## Return data with predictions
     pred_data <- list(pred_con_test = continuous_predictions$test,
                       pred_cat_test = categorical_predictions$test,
-                      tc = prepped_data$tc,
-                      outcome_test = prepped_data$y_review)
+                      tc = prepped_sample$tc,
+                      outcome_test = prepped_sample$y_review)
     ## Save all predictions
     if (save_all_predictions) {
         pred_data$pred_con_train <- continuous_predictions$train
         pred_data$pred_cat_train <- categorical_predictions$train
-        pred_data$outcome_train <- prepped_data$y_train   
+        pred_data$outcome_train <- prepped_sample$y_train   
     }
     if (verbose) message("Returning prediction data \n")
     return (pred_data)
