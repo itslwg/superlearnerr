436653f74b5cc59832d3bc7dfcf0be2b0d389e6d
janejkanske <ludviggerdin@gmail.com>
2018-05-25 21:05:55 +0200

    Fixed pretty names for ROC of all learners

--- R/create.ROCR.all.r
+++ R/create.ROCR.all.r
@@ -7,16 +7,16 @@ create.ROCR.all <- function(
                             study_sample
                             )
 {
-    models_data <- readRDS("C:\\Users\\Ludvig Gerdin\\Documents\\Projects\\SuperLearner\\SupaLarna\\superlearner.rds")$library.predict
-    models <- colnames(models_data)
+    ## Get predictions of SL learners from training set
+    models_data <- readRDS("./superlearner.rds")$library.predict
+    ## Initiate vector with Titles for plot
     pretty_names <- c("GLMnet",
                       "GLM",
                       "Random Forest",
                       "XGboost",
                       "GAM")
-    ## Initiate list to populate with dataframe columns
+    ## Initiate list to populate with dataframe columns and, then, fill
     l_of_predictions <- list()
-    ## Data frame columns to list
     for (i in colnames(models_data)){
         l_of_predictions[[i]] <- models_data[, i]
     }
@@ -32,7 +32,7 @@ create.ROCR.all <- function(
         pretty_name <- pretty_names[grep(model, models)]
         new_data <- cbind(data@y.values[[1]], data@x.values[[1]])
         new_data <- data.frame(new_data,
-                               rep(model, nrow(new_data)))
+                               rep(pretty_name, nrow(new_data)))
         colnames(new_data) <- c("tpr", "fpr","pretty_name")
         return(new_data)
     }))
@@ -41,7 +41,7 @@ create.ROCR.all <- function(
     linetypes <- c("solid", "dashed", "dotted")
     roc.plot <- function(plot_data) {
         plot_object <- ggplot(data = plot_data) +
-            geom_line(aes(x = fpr, y = tpr, col = pretty_name), size = 0.5, alpha = 0.8) +
+            geom_line(aes(x = fpr, y = tpr, col = pretty_name), size = 1, alpha = 0.8) +
             xlab("False positive rate") +
             ylab("True positive rate") +
             scale_color_manual(name = "", values = colors) +
@@ -50,7 +50,6 @@ create.ROCR.all <- function(
                   strip.text = element_text(size = 20, hjust = 0))
         return(plot_object)
     }
-    roc.plot(plot_data)
     roc <- roc.plot(plot_data)
     ## Save plots
     plot_name <- "roc_plot_all.pdf"
