fca9e8caea7cf4e38641825f5ca672734ea5c44d
janejkanske <ludviggerdin@gmail.com>
2018-07-21 15:54:55 +0200

    Adjusted for prediction_models_vs_clinicians
    Former-commit-id: 06c8f19a1b84d3a342955b88267a09a226f76210

---
+++ R/create.ROCR.plots.v2.r
@@ -0,0 +1,78 @@
+#' Create ROCR plots
+#'
+#' Plots reciever operating charecteristiqs curves or precision/recall curves.
+#' @param study_sample Study sample list. No default.
+#' @param device Character vector with the name of the image device to use. Passed to rocr.plot (in turn, passed to save.plot). Defaults to "eps".
+#' @param train_test Logical. Is the dataset splitted in train and test set? Defaults to TRUE.
+#' @param split_var The variable used to split plots. As string. Defaults to "train".
+#' @param ROC_or_precrec String. To perform ROC or precision/recall analysis. Accepted values are "ROC" or "prec_rec". No default.
+#' @param models Model names as character vector. Defaults to c("pred_con_train", "pred_cat_train", "pred_con_test", "pred_cat_test", "tc")
+#' @param pretty_names Names to be used in plots. As character vector. Defaults to c("SuperLearner continuous prediction", "SuperLearner priority levels", "SuperLearner continuous prediction", "SuperLearner priority levels", "Clinicians priority levels")
+#' @param subscript Logical. If TRUE, underscores in pretty names in converted to expression. Passed to rocr.plots. Defaults to FALSE.
+#' @export
+create.ROCR.plots <- function(
+                              study_sample,
+                              outcome_name,
+                              device = "eps",
+                              split_var = "train",
+                              train_test = TRUE,
+                              ROC_or_precrec = "ROC",
+                              models = c("pred_con_train",
+                                         "pred_cat_train",
+                                         "pred_con_test",
+                                         "pred_cat_test",
+                                         "tc"),
+                              pretty_names = c("SuperLearner continuous prediction",
+                                               "SuperLearner priority levels",
+                                               "SuperLearner continuous prediction",
+                                               "SuperLearner priority levels",
+                                               "Clinicians priority levels"),
+                              subscript = FALSE
+                              )
+{
+    ## Error handling
+    if (!(ROC_or_precrec %in% c("ROC", "prec_rec"))) stop("Accepted values for ROC_or_precrec argument is ROC and prec_rec")
+    ## Define setting depending on type of plot
+    if (ROC_or_precrec == "ROC") measures <- list(tpr = "tpr",
+                                                  fpr = "fpr",
+                                                  TPR = "True positive rate",
+                                                  FPR = "False positive rate")
+    if (ROC_or_precrec == "prec_rec") measures <- list(prec = "prec",
+                                                       rec = "rec",
+                                                       PREC = "Precision",
+                                                       REC = "True positive rate (recall)")
+    ## Get tpr and fpr (or precision)
+    analysis_comps <- lapply(setNames(nm = models), function(model) {
+        ## Set outcome according to set outcomes (necessary when sample is split)
+        outcome <- outcome_name
+        if (train_test) {
+            outcome <- "outcome_test"
+            if (grep("test", model)) outcome <- "outcome_test"
+        }
+        pred <- ROCR::prediction(as.numeric(study_sample[[model]]), study_sample[[outcome]])
+        perf <- ROCR::performance(pred, measure = measures[[1]], x.measure = measures[[2]])
+        return(perf)
+    })
+    ## Create plot data
+    plot_data <- do.call(rbind, lapply(setNames(nm = models), function(model) {
+        data <- analysis_comps[[model]]
+        pretty_name <- pretty_names[grep(model, models)]
+        set <- "B"
+        if (!grepl(split_var, model)) set <- "A"
+        new_data <- cbind(data@y.values[[1]], data@x.values[[1]])
+        new_data <- data.frame(new_data, rep(set, nrow(new_data)), rep(pretty_name, nrow(new_data)))
+        colnames(new_data) <- c(measures[[1]], measures[[2]], "set", "pretty_name")
+        return(new_data)
+    }))
+    ## Create and save plots
+    if (ROC_or_precrec  == "ROC") plot_name <- "roc_plot"
+    if (ROC_or_precrec  == "prec_rec") plot_name <- "prec_rec_plot"
+    rocr.plot(plot_data = plot_data,
+              y_name = measures[[1]],
+              x_name = measures[[2]],
+              ylab = measures[[3]],
+              xlab = measures[[4]],
+              device = device,
+              file_name = plot_name,
+              subscript = subscript)
+}
--- R/gridsearch.breaks.r
+++ R/gridsearch.breaks.r
@@ -2,7 +2,8 @@
 #'
 #' Performs a gridsearch to identify breaks to optimise a provided loss function
 #' @param predictions A numeric vector of predicted probabilites. No default.
-#' @param outomes A numeric vector of 0 and 1. No default.
+#' @param outcomes A numeric vector of 0 and 1. No default.
+#' @param grid All values to be combnd. As numeric vector. Defaults to seq(0.01, 0.99, 0.01).
 #' @param n_breaks Number of breaks, i.e. cutpoints. Defaults to 3.
 #' @param loss_function The name of the loss function as a character vector. For now any performance measure included in ROCR is accepted. Defaults to "auc".
 #' @param sample Logical. If TRUE loss is only calculated for 10% of possible breaks. Defaults to TRUE.
@@ -10,10 +11,12 @@
 #' @param n_cores Integer. The number of cores to use for parallel computing. Defaults to NULL.
 #' @param save_plot Logical. If TRUE a wireframe plot is saved to disk. Defaults to FALSE.
 #' @param verbose Logical. If TRUE then information is printed as the gridsearch runs. Defaults to FALSE. Currently ignored.
+#' @importFrom foreach %dopar%
 #' @export
 gridsearch.breaks <- function(
                               predictions,
                               outcomes,
+                              grid = seq(0.01, 0.99, 0.01),
                               n_breaks = 3,
                               loss_function = "auc",
                               sample = TRUE,
@@ -23,16 +26,16 @@ gridsearch.breaks <- function(
                               verbose = FALSE
                               )
 {
-    ## Verify that predictions appear to be probabilites
-    if (min(predictions) < 0 | max(predictions) > 1) stop("Predictions are not probabilites")
+    ## Verify if predictions appear to be probabilites.
+    if (min(predictions) < 0 | max(predictions) > 1) stop ("Predictions is not probabilities")
     ## Verify that outcomes is a vector of binary data with 0 and 1
     if (!all(levels(as.factor(outcomes)) %in% c("0", "1"))) stop("Outcomes is not a vector of binary data with only 0 and 1")
     ## Verify that there are no missing values in predictions or outcomes
-    if (!all(!is.na(predictions)) | !all(!is.na(predictions))) stop("There is missing data in predictions or outcomes")
+    if (!all(!is.na(predictions)) | !all(!is.na(outcomes))) stop("There is missing data in predictions or outcomes")
     ## Loss functions
     loss_functions <- list(auc = function(pred) ROCR::performance(pred, "auc", x.measure =  "cutoff")@y.values[[1]])
     ## Define grid
-    breaks_to_search <- combn(seq(0.01, 0.99, 0.01), n_breaks, simplify = FALSE)
+    breaks_to_search <- combn(grid, n_breaks, simplify = FALSE)
     ## Draw a random sample if sample is TRUE
     if (sample) breaks_to_search <- breaks_to_search[sample(1:length(breaks_to_search), ceiling(length(breaks_to_search) * 0.1))]
     message("Estimating loss for each of ", length(breaks_to_search), " possible cutpoints combinations")
@@ -50,12 +53,15 @@ gridsearch.breaks <- function(
             message("You have not specified the number of cores so 2 will be used")
             n_cores <- 2
         }
-        parallel_breaks <- split(breaks_to_search, cut(1:length(breaks_to_search), breaks = n_cores, include.lowest = TRUE))
-        cl <- makeCluster(n_cores)
-        registerDoParallel(cl)
+        parallel_breaks <- split(breaks_to_search,
+                                 cut(1:length(breaks_to_search),
+                                     breaks = n_cores,
+                                     include.lowest = TRUE))
+        cl <- parallel::makeCluster(n_cores)
+        doParallel::registerDoParallel(cl)
         message("Running gridsearch for optimal cutpoints in parallel on ", n_cores, " cores")
-        parallel_list <- foreach(breaks_to_search = parallel_breaks) %dopar% lapply(breaks_to_search, estimate.loss)
-        stopCluster(cl)
+        parallel_list <- foreach::foreach(breaks_to_search = parallel_breaks) %dopar% lapply(breaks_to_search, estimate.loss)
+        parallel::stopCluster(cl)
         loss_list <- do.call(c, parallel_list)
     } else loss_list <- lapply(breaks_to_search, estimate.loss)
     ## Loss data
