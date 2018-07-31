40c5d55f9c67ef015377bbf7ca5f0b03407a70c9
janejkanske <ludviggerdin@gmail.com>
2018-07-31 22:32:22 +0200

    Minor chanages

--- R/model.review.reclassification.r
+++ R/model.review.reclassification.r
@@ -20,11 +20,10 @@ model.review.reclassification <- function(
         ## Invert models given by models_to_invert
         p.new <- study_sample[[model]]
         if (model %in% models_to_invert){
+            print (model)
             p.new <- factor(study_sample[[model]])
             levels(p.new) <- c(4,3,2,1)
-            print (str(p.new))
             p.new <- as.numeric(p.new)
-            print(str(p.new))
         }
         reclass <- nricens::nribin(event = study_sample[[outcome_name]],
                                    p.std = study_sample$tc,
