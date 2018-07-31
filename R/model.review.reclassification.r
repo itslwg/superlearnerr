f8100498fe4c71fa1d74fd40d28a78761945aa96
janejkanske <ludviggerdin@gmail.com>
2018-07-31 21:22:59 +0200

    Added invert parameter

--- R/model.review.reclassification.r
+++ R/model.review.reclassification.r
@@ -4,38 +4,47 @@
 #' @param study_sample Sample as data frame.  No default
 #' @param model_names Names of list elements with model predictions in study_sample. Character vector. No default.
 #' @param outcome_name Name of outcome variable. As string. Defauls to "s30d".
+#' @param models_to_invert Character vector. Names of models to invert. Defaults to NULL.
 #' @export
 model.review.reclassification <- function(
                                           study_sample,
                                           model_names,
-                                          outcome_name = "s30d"
+                                          outcome_name = "s30d",
+                                          models_to_invert = NULL
                                           )
 {
     ## Error handling
     if (!("tc" %in% names(study_sample))) stop ("Triage category variable not in dataset.")
-    ## Compute reclassification of SuperLearner model and clinicians
-    reclassification_objs <- lapply(model_names,
-                                    function(model)
-                                        nricens::nribin(event = study_sample[[outcome_name]],
-                                                        p.std = study_sample$tc,
-                                                        p.new = study_sample[[model]],
-                                                        cut = sort(unique(study_sample$tc)),
-                                                        niter = 0,
-                                                        msg = FALSE
-                                                        )
-                                    )
+    ## Compute reclassification of models and clinicians
+    reclassification_objs <- lapply(model_names, function(model){
+        ## Invert models given by models_to_invert
+        p.new <- study_sample[[model]]
+        if (model %in% models_to_invert){
+            p.new <- factor(study_sample[[model]])
+            levels(p.new) <- c(4,3,2,1)
+            print (str(p.new))
+            p.new <- as.numeric(p.new)
+            print(str(p.new))
+        }
+        reclass <- nricens::nribin(event = study_sample[[outcome_name]],
+                                   p.std = study_sample$tc,
+                                   p.new = p.new,
+                                   cut = sort(unique(study_sample$tc)),
+                                   niter = 0,
+                                   msg = FALSE)
+        return (reclass)
+    })
     ## Extract nri dfs from nri objects and coerce to lists
-    reclassification_lsts <- lapply(setNames(reclassification_objs, nm = model_names),
-                                    function (obj) {
-                                        ## Subset list from nri object
-                                        nri_df <- obj$nri
-                                        ## Corce nri data frame to list
-                                        as_list <- setNames(as.list(nri_df[, "Estimate"]),
-                                                            nm = rownames(nri_df))
+    reclassification_lsts <- lapply(setNames(reclassification_objs, nm = model_names), function (obj) {
+        ## Subset list from nri object
+        nri_df <- obj$nri
+        ## Corce nri data frame to list
+        as_list <- setNames(as.list(nri_df[, "Estimate"]),
+                            nm = rownames(nri_df))
 
-                                        return (as_list)
-                                    }
-                                    )
+        return (as_list)
+    }
+    )
 
     return (reclassification_lsts)
 }
