109c29569a978d5cfc1230837a79c6d4f16c6c94
janejkanske <ludviggerdin@gmail.com>
2018-07-31 21:22:59 +0200

    Added invert parameter
    Former-commit-id: f8100498fe4c71fa1d74fd40d28a78761945aa96

--- R/model.review.with.rocr.r
+++ R/model.review.with.rocr.r
@@ -5,16 +5,20 @@
 #' @param outcome_name The name of the outcome variable. No default.
 #' @param model_names Character vector with list name of predictions. Default: c('pred_cat', 'tc')
 #' @param measure String. Performance measure for ROCR prediction object. Defaults to "auc".
+#' @param invert Logical. If TRUE, 1 - measure is returned. Defaults to FALSE.
+#' @param models_to_invert Character vector. Names of models to invert. Has to be given if invert is given. Defaults to NULL.
 #' @export
  model.review.with.rocr <- function(
                                     study_sample,
                                     outcome_name,
                                     model_names= c('pred_cat', 'tc'),
-                                    measure = "f"
+                                    measure = "f",
+                                    models_to_invert = NULL
                                     )
  {
      ## Generate AUROCC of models
-     perf_lst <- lapply(setNames(nm = model_names), function(model_name) {
+     perf_lst <- lapply(setNames(nm = model_names), function(model_name,
+                                                             models_to_invert) {
          ## Setup ROCR prediction object
          pred <- ROCR::prediction(as.numeric(study_sample[[model_name]]),
                                   study_sample[[outcome_name]])
@@ -25,6 +29,8 @@
          ## Subset y value(s) and x.value(s)
          y.values <- perf@y.values[[1]]
          x.values <- NULL
+         ## Modify y values if invert
+         if (model_name %in% models_to_invert) y.values <- 1 - y.values
          ## Append to x.values if length is not 0, i.e.
          ## names of cut points exist
          if (length(perf@x.values) != 0){
@@ -38,13 +44,13 @@
          ## Adjust for output without x.values, without
          ## for example x cut points
          if (is.null(x.values)){
-             return (perf@y.values[[1]])
+             return (y.values)
          } else {
              ## Coerce to individual lsts for each value
              ind_lst <- setNames(as.list(y.values),
                                  nm = paste0("cp_at_", x.values))
              return (ind_lst)
          }
-     })
+     }, models_to_invert = models_to_invert)
      return (perf_lst)
 }
