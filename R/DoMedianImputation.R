065354664ef5325ac57937da518fe4b7bb0a8293
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2019-06-27 15:17:22 +0200

    Changed to use ifelse instead of multiple ifs
    Former-commit-id: 5f1d8f47bd264cae04758f5ebc0252af87f28e4b

--- R/DoMedianImputation.R
+++ R/DoMedianImputation.R
@@ -1,25 +1,22 @@
 #' Do median imputation function
 #'
 #' Performs median imputation on variables with missing data
-#' @param study_data The study data frame. No default.
-#' @param missing_indicator A character vector of length 1 with the string used to idenfity variables with missing data to be imputed. Defaults to "_missing"
+#' @param study.sample Data frame. The study sample. No default.
+#' @param missing.indicator String. String used to idenfity variables with missing data to be imputed. Defaults to "_missing"
 #' @export
-do.median.imputation <- function(
-                                 study_data,
-                                 missing_indicator = "_missing"
-                                 )
-{
+DoMedianImputation <- function(study.sample, missing.indicator = "_missing") {
     ## Error handling
-    if (!is.data.frame(study_data)) stop("Study data is not a data frame")
-    if (length(missing_indicator) != 1) stop("Missing indicator length is not 1")
+    if (!is.data.frame(study.sample)) stop("Study data is not a data frame")
+    if (length(missing.indicator) != 1) stop("Missing indicator length is not 1")
     ## Find variables to impute
-    variables_to_impute <- sub(missing_indicator, "", grep(missing_indicator, colnames(study_data), value = TRUE))
+    variables.to.impute <- sub(missing.indicator, "", grep(missing.indicator, colnames(study.sample), value = TRUE))
     ## Impute those variables
-    study_data[, variables_to_impute] <- lapply(study_data[, variables_to_impute], function(x) {
-        if (is.factor(x)) most_common_value <- names(which.max(table(x))) # If the variable is a factor replace missing with the most common level
-        if (is.numeric(x)) most_common_value <- median(x, na.rm = TRUE) # If the variable is numeric replace with median
-        x[is.na(x)] <- most_common_value # Replace missing
-        return(x)
+    study.sample[, variables.to.impute] <- lapply(study.sample[, variables.to.impute], function(var) {
+        most.common.value <- ifelse(is.factor(var), names(which.max(table(var))), median(var, na.rm = TRUE))
+##        if (is.factor(var)) most.common.value <- names(which.max(table(var))) # If the variable is a factor replace missing with the most common level
+##        if (is.numeric(var)) most.common.value <- median(var, na.rm = TRUE) # If the variable is numeric replace with median
+        var[is.na(var)] <- most.common.value # Replace missing
+        return(var)
     })
-    return(study_data)
+    return(study.sample)
 }
