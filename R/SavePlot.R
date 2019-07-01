09243b67c1cd80b18a3303590c130deca3649f4c
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2019-07-01 09:16:19 +0200

    Rewrite to google stye
    Former-commit-id: d79601c543caf11ea835ac372c824361e866e9d4

--- R/SavePlot.R
+++ R/SavePlot.R
@@ -1,31 +1,24 @@
 #' Save plot
 #'
 #' Wrapper for ggsave that also crops and converts to eps if necessary
-#' @param plot_object The ggplot object. No default.
-#' @param file_name The plot file name. No default.
-#' @param width The width of the plot in cm. Defaults to 13.2.
-#' @param height The height of the plot in cm. Default to 9.
-#' @param device Character vector with the name of the image device to use. Defaults to "eps".
-#' @param convert_pdf_to_eps Logical. If TRUE and device = "pdf" then an attempt to convert that pdf using pdftops to eps is made. Defaults to TRUE.
+#' @param plot.object The ggplot object. No default.
+#' @param file.name Character vector of length 1. The plot file name. No default.
+#' @param width Numeric vector of length 1. The width of the plot in cm. Defaults to 13.2.
+#' @param height Numeric vector of length 1. The height of the plot in cm. Default to 9.
+#' @param device Character vector of length 1. Name of the image device to use. Defaults to "eps".
+#' @param convert.pdf.to.eps Logical. If TRUE and device = "pdf" then an attempt to convert that pdf using pdftops to eps is made. Defaults to TRUE.
 #' @export
-save.plot <- function(
-                      plot_object,
-                      file_name,
-                      width = 13.2,
-                      height = width * 0.65,
-                      device = "eps",
-                      convert_pdf_to_eps = TRUE
-                      )
-{
+SavePlot <- function(plot.object, file.name, width = 13.2, height = width * 0.65,
+                     device = "eps", convert.pdf.to.eps = TRUE) {
     ## Setup device
-    font_device <- device
-    if (device == "eps") font_device = "postscript"
+    font.device <- device
+    if (device == "eps") font.device = "postscript"
     ## Generate full file name
-    full_file_name <- paste0(file_name, ".", device)
+    full.file.name <- paste0(file.name, ".", device)
     ## Setup ggplot arguments depending on device
-    ggargs <- list(filename = full_file_name,
-                   plot = plot_object,
-                   device = font_device,
+    ggargs <- list(filename = full.file.name,
+                   plot = plot.object,
+                   device = font.device,
                    width = width,
                    height = height,
                    units = "cm",
@@ -35,20 +28,20 @@ save.plot <- function(
         ggargs$device <- cairo_ps
     }
     ## Save plot
-    extrafont::loadfonts(device = font_device, quiet = TRUE)
+    extrafont::loadfonts(device = font.device, quiet = TRUE)
     do.call(ggplot2::ggsave, ggargs)
     ## Embed fonts
     ## embed_fonts(full_file_name, outfile = full_file_name, options = "-dEPSCrop")
     ## Report that plot is saved
-    message(paste0(full_file_name, " saved to disk"))
+    message(paste0(full.file.name, " saved to disk"))
     ## Convert to eps
-    if (convert_pdf_to_eps & device == "pdf") {
-        eps_name <- paste0(file_name, ".eps")
-        convert.to.eps <- function() {
-            system(paste("pdftops -eps", full_file_name, eps_name))
-            message(paste0(full_file_name, " converted to ", eps_name))
+    if (convert.pdf.to.eps & device == "pdf") {
+        eps.name <- paste0(file.name, ".eps")
+        ConvertToEps <- function() {
+            system(paste("pdftops -eps", full.file.name, eps.name))
+            message(paste0(full.file.name, " converted to ", eps.name))
         }
-        tryCatch(convert.to.eps(),
+        tryCatch(ConvertToEps(),
                  warning = function(w) print(w),
                  error = function(e) print(e))
     }
