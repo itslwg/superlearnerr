9b9934c19a8710bfc9cc78a5089c1f82b220558b
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2019-07-19 22:30:34 +0200

    Adjust for three way partitioning
    Former-commit-id: f166d6619cf43adb43a0ab6857d8079acedfed54

--- R/PartitionSample.R
+++ R/PartitionSample.R
@@ -1,28 +1,44 @@
 #' Partition Sample
 #'
-#' Partitions the sample using the caret package, i.e. splits the dataset in train and test sets.
+#' Partitions the sample using stratified random sampling of the outcome of interest. Attempts to maintain class distribution as observed in the study sample. Accepted number of partitions are 2 or 3. If n.partitions is 3, 1 - train.size) / 2 is partitioned into both the validation and test sets.
 #' @param study.sample Data frame. The study sample. No default
 #' @param outcome.variable.name Character vector of length 1. The name of the outcome variable of interest. Defaults to "s30d"
-#' @param ... Additional arguments for caret::createDataPartition.
+#' @param n.partitions Numeric vector of length 1. The number of partitions to create. Either 2 of 3. If  Defaults to 2.
+#' @param train.size Numeric vector of length 1. The proportion of the sample that goes into training set. Defaults to 0.6
 #' @export
 PartitionSample <- function(study.sample, outcome.variable.name = "s30d",
-                            ...) {
+                            n.partitions = 2, train.size = 0.6) {
     ## Error handling
     if (!is.character(outcome.variable.name) | !bengaltiger::IsLength1(outcome.variable.name))
         stop("outcome.variable.name has to be a character vector of length 1")
+    if (!(n.partitions %in% c(2, 3)))
+        stop("n.partitions must be either 2 or 3")
+    if (!is.factor(study.sample[, outcome.variable.name]))
+        stop("The outcome of interest must be a factor for the partitioning to work properly.")
     ## Get the indices for the training set
-    train.indices <- caret::createDataPartition(y = study.sample[, outcome.variable.name], ...)
-    outcome <- study.sample[, outcome.variable.name]
-    ## Change factor levels of the outcome to 0s and 1s, and make numeric
-    levels(outcome) <- c(0, 1)
-    outcome <- as.numeric(as.character(outcome))
-    set.list <- lapply(list(train = train.indices, test = -train.indices), function(indices) {
-        ## Remove clinicians triage and outcome from the features
-        x <- study.sample[indices, ]
-        ## Subset tc from the samples
-        tc <- x[, "tc"]
-        x <- x[, !(names(x) %in% c(outcome.variable.name, "tc"))]
-        return (list(x = x, y = outcome[indices], tc = tc))
+    train.indices <- caret::createDataPartition(y = study.sample[, outcome.variable.name],
+                                                p = train.size, list = FALSE)
+    set.list <- list(train = study.sample[train.indices, ],
+                     test = study.sample[-train.indices, ])
+    if (n.partitions == 3) {
+        validation.and.train.set <- set.list$test
+        ## Get new indices for the validation set
+        validation.indices <- caret::createDataPartition(y = validation.and.train.set[, outcome.variable.name],
+                                                         p = 0.5, list = FALSE)
+        set.list$validation <- validation.and.train.set[validation.indices, ]
+        ## Change the test size accordingly
+        set.list$test <- validation.and.train.set[-validation.indices, ]
+    }
+    set.list <- lapply(set.list, function(set) {
+        ## Subset tc and outcome from the samples
+        tc <- set$tc
+        outcome <- set[, outcome.variable.name]
+        ## Change factor levels of the outcome to 0s and 1s, and make numeric
+        levels(outcome) <- c(0, 1)
+        outcome <- as.numeric(as.character(outcome))   
+        ## Remove the outcome of interest and clincians triage from
+        set <- set[, !(names(set) %in% c(outcome.variable.name, "tc"))]
+        return (list(x = set, y = outcome, tc = tc))
     })
     return (set.list)
 }
--- R/PartitionTrainAndPredict.R
+++ R/PartitionTrainAndPredict.R
@@ -3,19 +3,22 @@
 #' Partitions the study sample, fits the model and makes predictions with SuperLearner.
 #' @param study.sample Data frame. The study.sample. No default.
 #' @param outcome.variable.name Character vector of length 1. The name of the outcome variable of interest. Defaults to "s30d".
+#' @param n.partitions Numeric vector of length 1. The number of partitions to create with PartitionSample. Accepted values are 2 or 3. If 2, a train and test set is created. If 3, train, validation, and test sets are created - the models is fitted on the training set, optimal breaks is gridsearched on the validation set, and the model is tested on the test set. Defaults to 2. 
 #' @param save.to.results Logical. If TRUE SuperLearner predictions, outcome and tc in each partition is saved to the Results list. Defaults to TRUE.
 #' @param models.names Character vector. The model names to stack in SuperLearner. Defaults to c("SL.gam", "SL.randomForest", "SL.nnet, SL.xgboost", "SL.svm") 
 #' @export
 PartitionTrainAndPredict <- function(study.sample, outcome.variable.name = "s30d",
-                                     save.to.results = TRUE, model.names = c("SL.randomForest")){
+                                     model.names = c("SL.randomForest"), n.partitions = 2, 
+                                     save.to.results = TRUE){
     ## Error handling
     if (!is.data.frame(study.sample))
         stop ("data must be of type data frame")
     ## Partition the sample, and return the separate partitions, the corresponding outcome
     ## for both sets and tc in both sets
+    n.partitions = 3
     partitions.outcome.and.tc <- PartitionSample(study.sample = study.sample,
                                                  outcome.variable.name = outcome.variable.name,
-                                                 p = 0.75, groups = 2, list = FALSE)
+                                                 n.partitions = n.partitions)
     message("Fitting SuperLearner...")
     ## Fit the model to the training data
     fitted.sl <- with(partitions.outcome.and.tc, SuperLearner::SuperLearner(Y = train$y, X = train$x,
@@ -24,15 +27,17 @@ PartitionTrainAndPredict <- function(study.sample, outcome.variable.name = "s30d
                                                                             method = "method.AUC",
                                                                             verbose = FALSE))
     con.list.labels <- paste0("con.model.", names(partitions.outcome.and.tc))
-    ## Make predictions on both the train and test partitions
+    ## Make predictions on all partitions
     predictions <- lapply(setNames(partitions.outcome.and.tc, nm = con.list.labels),
                           function (partition.list) predict(object = fitted.sl,
                                                             newdata = partition.list$x,
                                                             onlySL = TRUE)$pred)
-    message("Finding Optimal breaks for continuous probabilities...")
-    ## Gridsearch the optimal cut-points for the predicted probabilities on the training set 
-    optimal.breaks <- GridsearchBreaks(predictions = predictions$con.model.train,
-                                       outcome.vector = partitions.outcome.and.tc$train$y)
+    label <- ifelse(n.partitions == 2, "train", "validation")
+    message(paste("Finding Optimal breaks for continuous probabilities on the", label, "set..."))
+    ## Gridsearch the optimal cut-points for the predicted probabilities on
+    ## the appropriate set
+    optimal.breaks <- GridsearchBreaks(predictions = predictions[grepl(label, con.list.labels)][[1]],
+                                       outcome.vector = partitions.outcome.and.tc[[label]]$y)   
     ## Bin predictions made on the test set
     cut.list.labels <- paste0("cut.model.", names(partitions.outcome.and.tc))
     binned.predictions <- lapply(setNames(predictions, nm = cut.list.labels),
