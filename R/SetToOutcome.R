ef55fcc51a99ae6bd88f0a1edf06c9789087087e
Ludde <ludviggerdin@gmail.com>
2019-07-29 20:21:37 +0200

    Change to google style
    Former-commit-id: 7296f84b58c11557fd736accf1198386b7a11813

--- R/SetToOutcome.R
+++ R/SetToOutcome.R
@@ -1,18 +1,16 @@
-#' Change outcome function
+#' Set To Outcome
 #'
 #' This function changes patient outcome data if certain conditions are met. The outcome variable is set to dead if patients were dead on discharge after 30 days or if dead after 24 hours. Moreover, s30d is set to alive if coded alive and admitted to other hospital.
-#' @param study_data The study data as a data frame. No default
+#' @param study.sample Data frame. The study data. No default
 #' @export
-set.to.outcome <- function(
-                           study_data
-                           )
-{
+SetToOutcome <- function(study.sample) {
     ## Test that study data if a data frame, stop if not
-    if (!is.data.frame(study_data)) stop("study_data is not a data frame")
+    if (!is.data.frame(study.sample))
+        stop("study.sample must be a data frame")
     ## Set s30d to dead if we know that patients were dead on discharge or at 24
     ## hours
-    study_data[study_data$hd == 1 & !is.na(study_data$hd) | study_data$s24h == 1 & !is.na(study_data$s24h), "s30d"] <- "Yes"
+    study.sample[study.sample$hd == 1 & !is.na(study.sample$hd) | study.sample$s24h == 1 & !is.na(study.sample$s24h), "s30d"] <- "Yes"
     ## Finally remove s24h and hd from the dataset
-    study_data <- study_data[, -grep("s24h|hd", colnames(study_data))]
-    return (study_data)
+    study.sample <- study.sample[, -grep("s24h|hd", colnames(study.sample))]
+    return (study.sample)
 }
