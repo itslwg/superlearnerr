9f07279392cf71dce66de8c8e384f4896992818b
Ludde <ludviggerdin@gmail.com>
2019-10-27 15:21:06 +0100

    Add documentation and reformat a bit
    Former-commit-id: ac6279c9b0436df6c3607520620a5c0bcdb065bd

--- R/CreateMortalityTable.R
+++ R/CreateMortalityTable.R
@@ -3,12 +3,19 @@
 #' Table reporting number (%) of patients that died in each triage category.
 #' @param predictions.outcome.and.tc List. The predictions, outcome and clinicians triage in each partition. No default.
 #' @param digits Numeric vector of length 1. Number of digits to use in table. Defaults to 2.
-#' @param scores.to.invert Characer vector. Model scores to invert. Defaults to NULL.
+#' @param model.labels Character vector. The labels of the models in the predictions.outcome.and.tc list. If NULL the labels are set to those not equal to the outcome.label. Defaults to NULL.
+#' @param outcome.label Character vector of length 1. The list label of the outcome variable in predictions.outcome.and.tc. Defaults to "s30d".
+#' @param pretty.names Character vector. Model pretty names for the table. If NULL they are set are set as the model labels in predictions.outcome.and.tc. Defaults to NULL.
+#' @param scores.to.invert Character vector. Model scores to invert. Defaults to NULL.
+#' @param save.to.disk Logical. If TRUE the table is saved to disk. Defaults to FALSE.
+#' @param file.format Character vector of length 1.The format in which to save the table to disk. Has to be one of c("pdf", "rmd", "docx"). Defaults to "docx".
+#' @param ... Additional arguments for MakeTable. 
 #' @export
 CreateMortalityTable <- function(predictions.outcome.and.tc,
                                  digits = 2, model.labels = NULL,
                                  outcome.label = "s30d", pretty.names = NULL,
-                                 scores.to.invert = NULL, save.to.disk = FALSE) {
+                                 scores.to.invert = NULL, save.to.disk = FALSE,
+                                 file.format = "docx", ...) {
     if (!is.null(scores.to.invert)) {
         Inversion <- function(score) {
             score.as.factor <- as.factor(score)
@@ -18,9 +25,9 @@ CreateMortalityTable <- function(predictions.outcome.and.tc,
         for (score.label in scores.to.invert)
             predictions.outcome.and.tc[[score.label]] <- Inversion(predictions.outcome.and.tc[[score.label]])
     }
+    nms <- names(predictions.outcome.and.tc)
     if (is.null(model.labels))
-        model.labels <- names(predictions.outcome.and.tc)[!(names(predictions.outcome.and.tc) %in% "s30d")]
-    ## Error handling
+        model.labels <- nms[!(nms %in% outcome.label)]   
     tbl <- as.data.frame(do.call(cbind, lapply(model.labels, function(model.label) {
         freq.tbl <- table(predictions.outcome.and.tc[[model.label]],
                           predictions.outcome.and.tc[[outcome.label]])
@@ -35,6 +42,9 @@ CreateMortalityTable <- function(predictions.outcome.and.tc,
         colnames(tbl) <- model.labels
     }
     if (save.to.disk)
-        MakeTable(as.data.frame(tbl), "mortality.table")
+        MakeTable(df = tbl, "mortality.table",
+                  save.to.disk = save.to.disk,
+                  file.format = file.format,
+                  ...)
     return (tbl)
 }
