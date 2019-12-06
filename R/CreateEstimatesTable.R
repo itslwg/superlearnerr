0e5048e7495ef823d272daefe1817ba8f9daf8cc
Ludde <ludviggerdin@gmail.com>
2019-12-06 18:42:55 +0100

    Fix that footnote can be specified in CreateEstimateTable
    Former-commit-id: d1322c54ba1417a59dd179874940a2dce376df49

---
+++ R/CreateEstimatesTable.R
@@ -0,0 +1,48 @@
+#' Create Estimates Table
+#'
+#' Generates the data frame for
+#' @param estimates.with.ci Character vector. The performance estimates with correponding ci. No default.
+#' @param caption Type. Description. Default/No default. 
+#' @param pretty.colnames Character vector. The colnames of your choice. Defaults to c("AUROCC (95% CI)", "Model-Clinicians AUROCC difference (95% CI)", "Model-Model AUROCC difference (95% CI)")
+#' @param pretty.rownames Character vector. The rownames of your choice. Defaults to c("SuperLearner_CON", "SuperLearner_CAT", "Clinicians")
+#' @param return.table Logical. If TRUE the estimates table is returned. Defaults to FALSE.
+#' @param save.to.disk Logical. If TRUE the table is saved to disk. Defaults to TRUE.
+#' @param file.format Character vector of length 1.The format in which to save the table to disk. Has to be one of c("pdf", "rmd", "docx"). Defaults to "docx".
+#' @param ... Additional arguments for MakeTable.
+#' @export
+CreateEstimatesTable <- function(estimates.with.ci, table.name = "estimates.table",
+                                 caption = "AUROCC (95\\% CI), Model-model AUROCC difference (95\\% CI),
+                                            and Model-Clincians AUROCC difference (95\\% CI).",
+                                 pretty.colnames = c("AUROCC (95\\% CI)",
+                                                     "Model-Clinicians AUROCC difference (95\\% CI)",
+                                                     paste0("Model-Model",
+                                                            kableExtra::footnote_marker_symbol(1, "latex"),
+                                                            "AUROCC difference (95\\% CI)")),
+                                 pretty.rownames = c("SuperLearner\\textsubscript{CON}",
+                                                     "SuperLearner\\textsubscript{CAT}",
+                                                     "Clinicians"),
+                                 return.table = FALSE, save.to.disk = TRUE, file.format = "pdf",
+                                 ...) {
+    ## Extract model-model and model-clinicians AUC differences
+    diff.aucs <- estimates.with.ci[grep("diff", names(estimates.with.ci))]
+    ## Get indices with model-model AUC differences
+    model.clinicians.indexes <- grepl("clinician", names(diff.aucs))
+    ## Extract the AUC point estimates
+    point.estimates = estimates.with.ci[!(names(estimates.with.ci) %in% names(diff.aucs)) &
+                                        !grepl("nri", names(estimates.with.ci))]
+    # Make estimate table
+    df <- data.frame(point.estimates = point.estimates,
+                     model.clinicians.auc.difference = c(diff.aucs[model.clinicians.indexes], NA),
+                     model.model.auc.difference = c(diff.aucs[!model.clinicians.indexes], NA),
+                     row.names = pretty.rownames, stringsAsFactors = FALSE)
+    colnames(df) <- pretty.colnames
+    df[is.na(df)] <- "Not applicable"
+    ## Make estimates data frame with xtable
+    tbl <- MakeTable(df, return.table = return.table,
+                     table.name = table.name,
+                     save.to.disk = save.to.disk,
+                     file.format = file.format, ...)
+    cat (tbl)
+    if (return.table)
+        return (df)
+}
---
+++ R/MakeTable.R
@@ -0,0 +1,50 @@
+#' Make Table
+#'
+#' Wrapper for kableExtra::kable.
+#' @param df Data frame. A data frame. No default.
+#' @param table.name Character vector of length 1. Name/label of the table. Note that this is also Used as the file name if saved to disk. No default.
+#' @param footnote Character vector of length 1. Footnote for the table. Defaults to NULL.
+#' @param return.table Logical. If TRUE xtable print is returned. Defaults to FALSE.
+#' @param save.to.disk Logical. If TRUE the table is saved to disk. Defaults to FALSE.
+#' @param file.format Character vector of length 1.The format in which to save the table to disk. Has to be one of c("pdf", "rmd", "docx"). Defaults to "docx".
+#' @param ... Additional arguments for kableExtra::kable.
+#' @export
+MakeTable <- function(df, table.name, footnote = NULL,
+                      return.table = FALSE, save.to.disk = FALSE,
+                      file.format = "docx", ...) {
+    ## Error handling
+    if (!is.data.frame(df))
+        stop("df must be of type data frame")
+    if (!(file.format %in% c("docx", "rmd", "pdf")) | !bengaltiger::IsLength1(file.format)) 
+        stop("file.format has to be one of docx, rmd, or pdf")
+    preamble <- "library(kableExtra)\nlibrary(knitr)\n"
+    call <- paste0(preamble, getCall(...))
+    if (!is.null(footnote))
+        call <- paste(call, "%>% footnote(symbol = footnote)")
+    if (save.to.disk) {
+        file.name <- paste0(table.name, ".rmd")
+        md.call <- paste("```{r echo = FALSE, results = 'asis'} \n",
+                         call, "\n",
+                         "```")
+        write(md.call, file.name)
+        if (file.format != "rmd") {
+            output.format.list <- list(docx = "word_document",
+                                       pdf = "pdf_document",
+                                       html = "html_document")
+            rmarkdown::render(file.name, output_format = output.format.list[[file.format]],
+                              envir = new.env())
+            file.remove(file.name)
+        }
+    }
+    if (return.table)
+        return(tbl)
+}
+getCall <- function(...) {
+    Call  <- function(x, format, ...) {
+        mc <- substitute(list(df, "latex", ...), parent.frame())
+        mc[[1]] <- match.call()[-1][[1]][[1]]
+        return (mc)
+    }
+    test <- Call(kable(...))
+    return (deparse(test))
+}
