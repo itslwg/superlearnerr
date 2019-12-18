c81e1ffb21bfcefd806cec9704abeded5f09b088
Ludde <ludviggerdin@gmail.com>
2019-12-18 20:38:05 +0100

    Change to google style and simplify
    Former-commit-id: c2ea9b8786dd09a75f9ac093057a9a44bcefb9c2

--- R/CreateCoefficientsTable.R
+++ R/CreateCoefficientsTable.R
@@ -1,67 +1,74 @@
 #' Make learner weight, risk, and AUROCC table function
 #'
 #' This function generates a risk, learner weight and learner-AUROCC table.
-#' @param prepped_sample The prepped_sample list. No default.
-#' @param superlearner_object_path The path to the SuperLearner object as generated from the SuperLearner::SuperLearner() method. Default: "./superlearner.rds"
+#' @param prepped.sample The prepped.sample list. No default.
+#' @param superlearner.object.path The path to the SuperLearner object as generated from the SuperLearner::SuperLearner() method. Default: "./superlearner.rds"
 #' @export
-coefficients.table <- function(
-                               prepped_sample,
-                               superlearner_object_path = "./superlearner.rds"
-                               )
-{
-    ## User Mikkos answer at https://stackoverflow.com/questions/35790652/removing-words-featured-in-character-vector-from-string
-    removeWords <- function(str, stopwords) {
-        x <- unlist(strsplit(str, " "))
-        paste(x[!x %in% stopwords], collapse = " ")
-    }
+CreateCoefficientsTable <- function(prepped.sample, table.name = "coeff.risk"
+                                    superlearner.object.path = "./SuperLearner.rds",
+                                    caption = "10 fold cross validated risk and area under
+                                               the receiver operating curve characteristics
+                                               (AUROCC) in the training sample, weight, and
+                                               AUROCC in the test sample for SuperLearner and
+                                               each included learner",
+                                    pretty.colnames = c("AUROCC (95\\% CI)",
+                                                        "Model-Clinicians AUROCC difference (95\\% CI)",
+                                                        paste0("Model-Model",
+                                                               kableExtra::footnote_marker_symbol(1, "latex"),
+                                                               "AUROCC difference (95\\% CI)")),
+                                    pretty.rownames = c("SuperLearner", "GLMnet", "GLM",
+                                                        "Random Forest", "XGboost", "GAM"),
+                                    footnote = "The risk is equal to 1 - cross validated AUROCC.
+                                                The weight is the weight given to each learner in
+                                                the final SuperLearner. A weight of 0 means that the
+                                                learner was not included. Abbreviations: GAM Generalized
+                                                Additive Model, GLM Generalized Linear Model, XGboost
+                                                Extreme Gradient Boosting Machine.",
+                                    return.table = FALSE, save.to.disk = TRUE, file.format = "pdf", ...) {
     ## Load SuperLearner object
-    superlearner_object <- readRDS(superlearner_object_path)
+    superlearner.object <- readRDS(superlearner.object.path)
     ## Error handling
-    if (!is.list(superlearner_object)) stop("Superlearner object is not a list.")
+    if (!is.list(superlearner.object))
+        stop("Superlearner object is not a list.")
     ## Set learner names
-    learner_names <- unlist(lapply(superlearner_object$SL.library$library$predAlgorithm,
+    learner.names <- unlist(lapply(superlearner.object$SL.library$library$predAlgorithm,
                                    function(name) strsplit(name, ".", fixed = TRUE)))
-    learner_names <- learner_names[!(learner_names %in% "SL")]
-    ## Define pretty names
-    pretty_names <- c("SuperLearner",
-                      "GLMnet",
-                      "GLM",
-                      "Random Forest",
-                      "XGboost",
-                      "GAM")
+    learner.names <- learner.names[!(learner.names %in% "SL")]
     ## Get predictons on training set from each model
-    preds <- do.call(cbind, predict(superlearner_object, newdata = prepped_sample$x_review))
-    colnames(preds) <- pretty_names
+    preds <- do.call(cbind, predict(superlearner.object, newdata = prepped.sample$x.review))
+    colnames(preds) <- pretty.names
     ## Initiate list and save preds columns, i.e. model predictions, to list
-    l_of_predictions <- list()
-    for (i in colnames(preds)){
-        l_of_predictions[[i]] <- preds[, i]
-    }
+    l.of.predictions <- lapply(setNames(nm = colnames(preds)), function(nm) preds[, nm])
     ## Calculate AUC of learners
-    auroccs <- lapply(l_of_predictions,
-                      function (model){
-                          pred <- ROCR::prediction(model, prepped_sample$y_review)
-                          perf <- ROCR::performance(pred, measure = "auc", x.measure = "cutoff")@y.values
-                          return(perf)
-                      })
+    auroccs <- lapply(l.of.predictions, function(predictions) EvaluateWithRocr(predictions, prepped.sample$y.test))
     ## Set table
-    t_coeff_risk <- data.frame(Learner = pretty_names,
-                               cvRisk = c(NA, superlearner_object$cvRisk),
-                               cvAUROCC = 1 - c(NA, superlearner_object$cvRisk),
-                               Weight = c(NA, superlearner_object$coef),
+    t.coeff.risk <- data.frame(Learner = pretty.names,
+                               cvRisk = c("Not applicable", superlearner.object$cvRisk),
+                               cvAUROCC = 1 - c("Not applicable", superlearner.object$cvRisk),
+                               Weight = c("Not applicable", superlearner.object$coef),
                                AUROCC = unlist(auroccs),
                                stringsAsFactors = FALSE)
     ## Round columns
-    t_coeff_risk[] <- lapply(t_coeff_risk, function(x) if(!is.character(x)) sprintf("%.3f", x) else x)
-    colnames(t_coeff_risk) <- c("Learner", "Cross validated risk", "Cross validated AUROCC", "Weight", "AUROCC in test sample")
-    ## Format table
-    coeff_risk_table <- print.xtable(xtable(t_coeff_risk,
-                                            caption = "10 fold cross validated risk and area under the receiver operating curve characteristics (AUROCC) in the training sample, weight, and AUROCC in the test sample for SuperLearner and each included learner",
-                                            label = "tab:coeff_risk"),
-                                     include.rownames = FALSE,
-                                     caption.placement = "top",
-                                     print.results = FALSE)
-    coeff_risk_table <- add.star.caption(coeff_risk_table, "NA is not applicable. The SuperLearner was not cross validated, only the included learners were. The risk is equal to 1 - cross validated AUROCC. The weight is the weight given to each learner in the final SuperLearner. A weight of 0 means that the learner was not included. Abbreviations: GAM Generalized Additive Model, GLM Generalized Linear Model, XGboost Extreme Gradient Boosting Machine.")
-    return(coeff_risk_table)
+    t.coeff.risk[] <- lapply(t.coeff.risk, function(x) if(!is.character(x)) sprintf("%.3f", x) else x)
+    colnames(t.coeff.risk) <- c("Learner", "Cross validated risk", "Cross validated AUROCC", "Weight", "AUROCC in test sample")
+    MakeTable(t.coeff.risk,
+              table.name = table.name,
+              save.to.disk = save.to.disk,
+              file.format = file.format,
+              return.table = return.table
+              footnote = footnote, ...)
+    if (return.table)
+        return (t.coeff.risk)
 }
 
+#' RemoveWords
+#'
+#' Remove
+#' @param str Character vector of length 1. The string in which words are to be removed. No default.
+#' @param stopwords Character vector of length 1 or list of character vectors. The words to remove in str. No default.
+RemoveWords <- function(str, stopwords) {
+    ## User Mikkos answer at
+    ## https://stackoverflow.com/questions/35790652/removing-words-featured-in-character-vector-from-string
+    x <- unlist(strsplit(str, " "))
+    paste(x[!x %in% stopwords], collapse = " ")
+}
