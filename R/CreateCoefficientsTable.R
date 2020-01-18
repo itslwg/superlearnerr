c55488cf616e26ccae8ac45bc634c13305c636fd
Ludde <ludviggerdin@gmail.com>
2020-01-18 08:15:21 +0100

    Change according to new structure
    Former-commit-id: e34ca73fefc8553d6046842e775a22c08e832f88

--- R/CreateCoefficientsTable.R
+++ R/CreateCoefficientsTable.R
@@ -1,23 +1,32 @@
 #' Make learner weight, risk, and AUROCC table function
 #'
 #' This function generates a risk, learner weight and learner-AUROCC table.
-#' @param prepped.sample The prepped.sample list. No default.
+#' @param samples.and.predictions List. Partitions, predictions, and outcomes. No default.
+#' @param table.name Character vector of length 1. Used for the file name and label. Defaults to "coeff.risk"
 #' @param superlearner.object.path The path to the SuperLearner object as generated from the SuperLearner::SuperLearner() method. Default: "./superlearner.rds"
+#' @param caption Character vector of length 1. Used as caption for caption in rendered table. Defaults to  "10 fold cross validated risk and area under the receiver operating curve characteristics (AUROCC) in the training sample, weight, and AUROCC in the test sample for SuperLearner and each included learner",
+#' @param pretty.colnames Character vector. Column names for the rendered table. Defaults to c("AUROCC (95\\% CI)", "Model-Clinicians AUROCC difference (95\\% CI)", paste0("Model-Model", kableExtra::footnote_marker_symbol(1, "latex"), "AUROCC difference (95\\% CI)"))
+#' @param pretty.model.nms Character vector. Rownames for rendered table. Defaults to c("SuperLearner", "GLMnet", "GLM", "Random Forest", "XGboost", "GAM")
+#' @param footnote Character vector of length 1. Footnote for the table. Defaults to "The risk is equal to 1 - cross validated AUROCC. The weight is the weight given to each learner in the final SuperLearner. A weight of 0 means that the learner was not included. Abbreviations: GAM, Generalized Additive Model; GLM, Generalized Linear Model; XGboost, Extreme Gradient Boosting Machine"
+#' @param return.table Logical vector of length 1. If TRUE the function returns the table. Defaults to FALSE.
+#' @param save.to.disk Logical vector of length 1. If TRUE the table is saved to disk. Defaults to TRUE.
+#' @param file.format Character vector of length 1. File format used for the saved table. Defaults to "pdf"
+#' @param ... Additional arguments passes to MakeTable. 
 #' @export
-CreateCoefficientsTable <- function(prepped.sample, table.name = "coeff.risk"
+CreateCoefficientsTable <- function(sample, outcomes, table.name = "coeff.risk",
                                     superlearner.object.path = "./SuperLearner.rds",
                                     caption = "10 fold cross validated risk and area under
                                                the receiver operating curve characteristics
                                                (AUROCC) in the training sample, weight, and
                                                AUROCC in the test sample for SuperLearner and
                                                each included learner",
-                                    pretty.colnames = c("AUROCC (95\\% CI)",
-                                                        "Model-Clinicians AUROCC difference (95\\% CI)",
-                                                        paste0("Model-Model",
-                                                               kableExtra::footnote_marker_symbol(1, "latex"),
-                                                               "AUROCC difference (95\\% CI)")),
-                                    pretty.rownames = c("SuperLearner", "GLMnet", "GLM",
-                                                        "Random Forest", "XGboost", "GAM"),
+                                    pretty.colnames = c("Learner",
+                                                        "Cross validated risk",
+                                                        "Cross validated AUROCC",
+                                                        "Weight",
+                                                        "AUROCC in test sample"),
+                                    pretty.model.nms = c("SuperLearner", "GLMnet", "GLM",
+                                                         "Random Forest", "XGboost", "GAM"),
                                     footnote = "The risk is equal to 1 - cross validated AUROCC.
                                                 The weight is the weight given to each learner in
                                                 the final SuperLearner. A weight of 0 means that the
@@ -35,32 +44,31 @@ CreateCoefficientsTable <- function(prepped.sample, table.name = "coeff.risk"
                                    function(name) strsplit(name, ".", fixed = TRUE)))
     learner.names <- learner.names[!(learner.names %in% "SL")]
     ## Get predictons on training set from each model
-    preds <- do.call(cbind, predict(superlearner.object, newdata = prepped.sample$x.review))
-    colnames(preds) <- pretty.names
+    preds <- do.call(cbind, predict(superlearner.object, newdata = sample))
+    colnames(preds) <- pretty.model.nms
     ## Initiate list and save preds columns, i.e. model predictions, to list
-    l.of.predictions <- lapply(setNames(nm = colnames(preds)), function(nm) preds[, nm])
+    list.of.predictions <- lapply(setNames(nm = colnames(preds)), function(nm) preds[, nm])
     ## Calculate AUC of learners
-    auroccs <- lapply(l.of.predictions, function(predictions) EvaluateWithRocr(predictions, prepped.sample$y.test))
+    auroccs <- lapply(list.of.predictions, function(predictions) EvaluateWithRocr(predictions, outcomes))
     ## Set table
-    t.coeff.risk <- data.frame(Learner = pretty.names,
-                               cvRisk = c("Not applicable", superlearner.object$cvRisk),
-                               cvAUROCC = 1 - c("Not applicable", superlearner.object$cvRisk),
-                               Weight = c("Not applicable", superlearner.object$coef),
+    t.coeff.risk <- data.frame(Learner = pretty.model.nms,
+                               cvRisk = c(NA, superlearner.object$cvRisk),
+                               cvAUROCC = 1 - c(NA, superlearner.object$cvRisk),
+                               Weight = c(NA, superlearner.object$coef),
                                AUROCC = unlist(auroccs),
                                stringsAsFactors = FALSE)
-    ## Round columns
-    t.coeff.risk[] <- lapply(t.coeff.risk, function(x) if(!is.character(x)) sprintf("%.3f", x) else x)
-    colnames(t.coeff.risk) <- c("Learner", "Cross validated risk", "Cross validated AUROCC", "Weight", "AUROCC in test sample")
+    t.coeff.risk[is.na(t.coeff.risk)] <- "Not applicable"
+    t.coeff.risk[] <- lapply(t.coeff.risk, function(x) if (!is.character(x)) sprintf("%.3f", x) else x)
+    colnames(t.coeff.risk) <- pretty.colnames
     MakeTable(t.coeff.risk,
               table.name = table.name,
               save.to.disk = save.to.disk,
               file.format = file.format,
-              return.table = return.table
+              return.table = return.table,
               footnote = footnote, ...)
     if (return.table)
         return (t.coeff.risk)
 }
-
 #' RemoveWords
 #'
 #' Remove
