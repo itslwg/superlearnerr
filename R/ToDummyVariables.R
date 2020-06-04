ea1fb9e492eeedefd7b3e33dbb80723f41c4a727
Ludde <ludviggerdin@gmail.com>
2020-06-04 18:20:03 +0200

    Add composite to not.feature.variables
    Former-commit-id: 9b7160cc45751956a066bef7a2186cdc93340c8c

--- R/ToDummyVariables.R
+++ R/ToDummyVariables.R
@@ -1,9 +1,10 @@
 #' To dummy variables
 #'
 #' This function transforms factor variables to dummy variables.
-#' @param study.sample Type. Description. Default/No default. 
+#' @param study.sample data.frame. The study.sample. No default.
+#' @param not.feature.variables Character vector. Variables in study.sample not to be treated as features. Defaults to c("s30d", "tc", "composite").
 #' @export
-ToDummyVariables <- function(study.sample, not.feature.variables = c("s30d", "tc")) {
+ToDummyVariables <- function(study.sample, not.feature.variables = c("s30d", "tc", "composite")) {
     ## Error handling
     if (!is.data.frame(study.sample))
         stop("study.sample must be a data frame")
